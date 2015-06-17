4ddaf34b7e23fcab28a86f75ddb03a9c7ec7272e
Ioannis Filippidis <jfilippidis@gmail.com>
2015-06-17 00:14:11 -0700

    bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = 'README.md'
 VERSION_FILE = '{name}/_version.py'.format(name=name)
 MAJOR = 0
 MINOR = 0
-MICRO = 1
+MICRO = 2
 version = '{major}.{minor}.{micro}'.format(
     major=MAJOR, minor=MINOR, micro=MICRO)
 s = (

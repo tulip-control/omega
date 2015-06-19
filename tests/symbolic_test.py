613ba090b7b005af0a612552c2db4c52c4d0b8b8
Ioannis Filippidis <jfilippidis@gmail.com>
2015-06-18 19:59:00 -0700

    refactor test

--- tests/symbolic_test.py
+++ tests/symbolic_test.py
@@ -1,13 +1,13 @@
+from omega.logic import bitvector as bv
 from omega.symbolic import bdd as _bdd
-from omega.logic import bitvector
 
 
 def slugsin_parser(s, t):
     """Helper that converts prefix to infix syntax for readability."""
     slugs_table = t.to_slugs()
-    ltl_parser = bitvector.Parser()
+    ltl_parser = bv.Parser()
     p = ltl_parser.parse(s)
-    bitvector.add_bitnames(slugs_table)
+    bv.add_bitnames(slugs_table)
     s = p.flatten(t=slugs_table)
     slugsin_parser = _bdd.PrefixParser()
-    print slugsin_parser.parse(s)
+    print(slugsin_parser.parse(s))

2cda11911812a478c23f870bda5a15aea6d39d66
Ioannis Filippidis <jfilippidis@gmail.com>
2015-12-06 19:30:55 -0800

    DOC: correct comment

--- omega/logic/syntax.py
+++ omega/logic/syntax.py
@@ -201,7 +201,7 @@ def _compute_as_binary_tree_simple(f, x):
     assert len(x) > 0
     while len(x) > 1:
         n = len(x)
-        # consume the power of 2
+        # halve
         r = [f(a, b) for a, b in zip(x[::2], x[1::2])]
         # has last element ?
         if n % 2 == 1:

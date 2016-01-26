58d1e5e2b2cdee5e7a2e9e75d419fe09e81fd0bf
Ioannis Filippidis <jfilippidis@gmail.com>
2016-01-25 22:01:39 -0800

    REL: bump requirement to `dd >= 0.2.1`
    to use the methods `BDD.copy` and `BDD.rename`.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ s = (
     '# This file was generated from setup.py\n'
     "version = '{version}'\n").format(version=version)
 install_requires = [
-    'dd >= 0.2.0',
+    'dd >= 0.2.1',
     'ply >= 3.6',
     'natsort >= 3.5.3',
     'networkx >= 1.9.1']

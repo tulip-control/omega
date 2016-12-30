30e6ddfd6575787adc2238a5f4e053e124875bac
Ioannis Filippidis <jfilippidis@gmail.com>
2016-12-31 00:46:23 +0200

    REL: bump version to 0.0.11

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ README = 'README.md'
 VERSION_FILE = '{name}/_version.py'.format(name=name)
 MAJOR = 0
 MINOR = 0
-MICRO = 10
+MICRO = 11
 VERSION = '{major}.{minor}.{micro}'.format(
     major=MAJOR, minor=MINOR, micro=MICRO)
 VERSION_TEXT = (

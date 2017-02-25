218c786bb4b71950e813ef85f07849d3701ba713
Ioannis Filippidis <jfilippidis@gmail.com>
2017-02-24 20:10:02 -0800

    STY: shorter lines

--- omega/symbolic/fixpoint.py
+++ omega/symbolic/fixpoint.py
@@ -57,7 +57,8 @@ def trap(env_action, sys_action, safe, aut,
     """
     logger.info('++ cinv')
     bdd = aut.bdd
-    q = bdd.true  # if unless is not None, then q = safe is wrong
+    q = bdd.true  # if `unless is not None`,
+                  # then `q = safe` is wrong
     qold = None
     while q != qold:
         qold = q

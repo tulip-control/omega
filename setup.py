10429672ab594b73ef1e397cbc0f16679740cd6a
Ioannis Filippidis <jfilippidis@gmail.com>
2017-09-13 21:01:12 -0700

    REL: add Trove classifiers for Python 3.4, 3.5, 3.6
    Explicit is better than implicit [PEP 20].

--- setup.py
+++ setup.py
@@ -35,6 +35,9 @@ classifiers = [
     'Programming Language :: Python',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Topic :: Scientific/Engineering']
 keywords = [
     'first-order', 'propositional', 'logic',

0c70981f017eaf1629ec946f652eb8aa14c34133
Ioannis Filippidis <jfilippidis@gmail.com>
2020-02-14 22:23:35 +0200

    REL: add Trove classifier for Python 3.7

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ classifiers = [
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Topic :: Scientific/Engineering']
 keywords = [
     'first-order', 'propositional', 'logic',

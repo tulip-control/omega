tulip-control/omega

commit a1d48c1dc79e7c4b7e93d2f13cba9c42ed04f678

REL: require `setuptools >= 38.6.0` in `setup_requires`
to support a README written in Markdown [1].

[1] https://github.com/pypa/setuptools/blob/master/CHANGES.rst#v3860

setup.py
@@ -27,6 +27,7 @@ install_requires = [
     'networkx >= 2.0',
     'ply >= 3.6',
     'pydot >= 1.2.2']
+setup_requires=['setuptools >= 38.6.0'],
 tests_require = ['nose >= 1.3.4']
 classifiers = [
     'Development Status :: 2 - Pre-Alpha',
@@ -109,6 +110,7 @@ def run_setup():
         author_email='jfilippidis@gmail.com',
         url=url,
         license='BSD',
+        setup_requires=setup_requires,
         install_requires=install_requires,
         tests_require=tests_require,
         packages=[name, 'omega.games',

commit dbf64a3c61c08a94c521222669ad944302848e8d

BUG: rm erroneous comma
This comma resulted in `setup_requires` being
defined as a monuple with a `list` as element.

This error was introduced in commit
`a1d48c1dc79e7c4b7e93d2f13cba9c42ed04f678`.

setup.py
@@ -29,7 +29,7 @@ install_requires = [
     'networkx >= 2.0',
     'ply >= 3.6, <= 3.10',
     'pydot >= 1.2.2']
-setup_requires = ['setuptools >= 38.6.0'],
+setup_requires = ['setuptools >= 38.6.0']
 tests_require = ['nose >= 1.3.4']
 classifiers = [
     'Development Status :: 2 - Pre-Alpha',